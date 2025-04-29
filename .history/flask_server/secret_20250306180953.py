b40c79a3bd48bb312d533a9d5e156753289d46a5
zachd1234 <131545095+zachd1234@users.noreply.github.com>
2025-04-29 13:38:01 -0700

    cleanup

--- .history/flask_server/secret_20250306180953.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Set environment variables for API keys
-os.environ['APOLLO_API_KEY'] = 'e-txDJ7c5zArGtz9R0LCcA'
-os.environ["OPENAI_API_KEY"] = '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'
-os.environ["HUNTER_API_KEY"] = 'e826a3bff94e605a3edc045f43bde918709d30b3'
-os.environ["GOOGLE_API_KEY"] = 'AIzaSyAAwJNQQtFbxEkZkhxBX7ubbOZ7ioolxiE'
-
-# Note: You might need additional configuration variables depending on the application requirements 
